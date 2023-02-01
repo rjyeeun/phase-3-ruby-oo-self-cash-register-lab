70599c6b1712e0e7c5a457734a979ce31b1e7dac
Yeeun Jung <rjyeeun@gmail.com>
2023-02-01 12:07:00 -0500

    commit tree ae34dc8544613c1f4266fd56df6a777aefa819c6

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+require 'pry'
+class CashRegister 
+    attr_accessor :total, :items
+    attr_reader :discount
+
+    def initialize(discount =0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+        quantity.times do
+        @items << title 
+        end
+
+    end
+
+    def apply_discount
+        if discount > 0
+            my_discount = @total * @discount/100
+            @total = total - my_discount
+            "After the discount, the total comes to $#{@total}."
+        else "There is no discount to apply."
+        end
+    end
+
+
+end
+
+binding.pry
+
+
+
+
+
 
